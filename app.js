9c201312d7c0ede001d0914f6a2b925f2da24a2c
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-05-04 21:28:01 -0400

    you can now sign up and working in on log in

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const layouts      = require('express-ejs-layouts');
-const mongoose     = require('mongoose');
-
-
+const bodyParser = require('body-parser');
+const layouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const User = require('./models/user-model.js');
 mongoose.connect('mongodb://localhost/passport-app');
 
 const app = express();
@@ -23,32 +24,65 @@ app.locals.title = 'Express - Generated with IronGenerator';
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
+app.use(session({
+    secret: 'my cool passport app',
+    resave: true,
+    saveUninitialized: true
+}));
+// it has to be after the session -express
+app.use(passport.initialize());
+app.use(passport.session());
+//..... and before your routes
+
+//determines what to save in the session what put in the box
+passport.serializeUser((user, cb) => {
+  //only when you log in
+    cb(null, user._id);
+});
+
+//where to get the rest of the users given (what in the box)
+passport.deserializeUser((userId, cb) => {
+  //callled every time AFTER LOG IN
+  //querying the database with the id
+    User.findById(userId, (err, theUser) => {
+        if (err) {
+            cb(err);
+            return;
+        }
+        // we are sending user info to passport
+        cb(null, theUser);
+    });
+});
+
+
 // ================================================================================
 const index = require('./routes/index');
 app.use('/', index);
 const myAuthRoutes = require('./routes/auth-route.js');
-app.use('/',myAuthRoutes);
+app.use('/', myAuthRoutes);
 // ================================================================================
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
